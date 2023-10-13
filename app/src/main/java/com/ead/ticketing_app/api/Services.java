eceafe136176938d756b986ecd22d2218c91d41b
maneeshalakshani <maneeshalakshani9@gmail.com>
2023-10-13 21:04:15 +0530

    feat: API calls

--- app/src/main/java/com/ead/ticketing_app/api/Services.java
+++ app/src/main/java/com/ead/ticketing_app/api/Services.java
@@ -1,5 +1,10 @@
 package com.ead.ticketing_app.api;
 
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+
 import com.ead.ticketing_app.custom_classes.CustomTrustManager;
 
 import java.io.IOException;
@@ -8,6 +13,8 @@ import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -39,110 +46,4 @@ public class Services {
             return new OkHttpClient(); // Return a default client if an error occurs
         }
     }
-
-
-    public String fetchData() {
-        OkHttpClient client = createCustomOkHttpClient();
-
-        //generate request
-        Request request = new Request.Builder()
-                .url(API_URL + "Booking")
-                .build();
-
-        try {
-            //getting responses
-            Response response = client.newCall(request).execute();
-
-            //check responses
-            if (response.isSuccessful()) {
-                return response.body().string();
-            } else {
-                // Handle the error response
-                return null;
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-//    public void addBooking() {
-//        OkHttpClient client = createCustomOkHttpClient();
-//
-//        // Define the POST request body (e.g., form data)
-//        RequestBody requestBody = new FormBody.Builder()
-//                .add("fromStation", "Colombo")
-//                .add("toStation", "Matara")
-//                .add("journeyDate", "12/06/2023")
-//                .add("noOfTickets", "12")
-//                .add("ticketclass", "class 1")
-//                .build();
-//
-//        Request request = new Request.Builder()
-//                .url(API_URL + "Booking/addBooking/")
-//                .post(requestBody)
-//                .build();
-//
-//        client.newCall(request).enqueue(new Callback() {
-//            @Override
-//            public void onFailure(Call call, IOException e) {
-//                e.printStackTrace();
-//            }
-//
-//            @Override
-//            public void onResponse(Call call, Response response) throws IOException {
-//                if (response.isSuccessful()) {
-//                    String responseBody = response.body().string();
-//                    // Handle the successful response
-//                    Log.d("Response:", responseBody);
-//                } else {
-//                    Log.e("Error:", "Failed to send POST request");
-//                }
-//            }
-//        });
-//    }
-
-    public String addBooking() throws IOException {
-        OkHttpClient client = createCustomOkHttpClient();
-
-        // Define the JSON request body
-        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
-        String jsonBody = "{"
-                + "\"fromStation\":\"Colombo\","
-                + "\"toStation\":\"Matara\","
-                + "\"journeyDate\":\"12/06/2023\","
-                + "\"noOfTickets\":12,"
-                + "\"ticketclass\":\"1\""
-                + "}";
-        RequestBody requestBody = RequestBody.create(jsonBody, JSON);
-
-        Request request = new Request.Builder()
-                .url("https://10.0.2.2:7104/api/Booking/addBooking")
-                .addHeader("accept", "text/plain")
-                .addHeader("Content-Type", "application/json")
-                .post(requestBody)
-                .build();
-
-        Response response = client.newCall(request).execute();
-        return response.body().string();
-
-//        client.newCall(request).enqueue(new Callback() {
-//            @Override
-//            public void onFailure(Call call, IOException e) {
-//                e.printStackTrace();
-//            }
-//
-//            @Override
-//            public void onResponse(Call call, Response response) throws IOException {
-//                if (response.isSuccessful()) {
-//                    String responseBody = response.body().string();
-//                    // Handle the successful response
-//                    Log.d("Response:", responseBody);
-//                } else {
-//                    Log.e("Error:", "Failed to send POST request");
-//                }
-//            }
-//        });
-    }
-
 }
